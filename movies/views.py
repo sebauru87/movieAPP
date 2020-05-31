8e52429b00b6032fc87cb0195f0178d463189db2
sebauru87 <64660679+sebauru87@users.noreply.github.com>
2020-05-30 23:42:16 -0300

    modal creat added

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Movie
 # Create your views here.
@@ -6,5 +6,8 @@ from .models import Movie
 def home_page(request):
     user_query = str(request.GET.get('query', ''))
     search_result = Movie.objects.filter(name__icontains=user_query)
-    stuff_for_frontend = {"search_result": search_result}
+    stuff_for_frontend = {'search_result': search_result}
     return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
+
+def create(request):
+    return redirect('/')
