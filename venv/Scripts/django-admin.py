ab06aa747e16fd5cf41a6b33899caeb71ff62bae
Sebastian <Ferreira>
2020-06-10 23:58:23 -0300

    remove from github venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\sebau\Documents\progra\cleverprogrammer\movieAPP\venv\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
