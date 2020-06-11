eec9c1a54e0313ea57376b6225f776f8093a0c64
Sebastian <Ferreira>
2020-06-11 00:48:13 -0300

    change debug false, and allowed host without http

--- cp_django_movie_app/settings.py
+++ cp_django_movie_app/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://movieapppython.herokuapp.com/' ,'127.0.0.1']
+ALLOWED_HOSTS = ['movieapppython.herokuapp.com/' ,'127.0.0.1']
 
 
 # Application definition
